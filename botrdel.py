598dddf414b71256926a01cd807a896fce9e0c5a
deblokc <74002970+deblokc@users.noreply.github.com>
2020-11-06 14:18:46 +0100

    Update botrdel.py

--- botrdel.py
+++ botrdel.py
@@ -33,7 +33,7 @@ async def on_ready():
 @client.event
 async def on_message(message): #quand un message est envoyé 
     if message.author != client.user: #on vérifie que ce n'est pas un message du bot
-        if "<@!bot>" in message.content.split(): #on vérifie que le bot à été ping
+        if "<@!689429696093356054>" in message.content.split(): #on vérifie que le bot à été ping
             liste=[]
             channel = discord.utils.get(client.get_all_channels(), guild__name='Ché Mwa', name='message') #on sélectionne le channel dans lequel on va prendre les messages
             async for texte in channel.history(limit=20):
