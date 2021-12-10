2043b5a91ad5b5aae9cecc735f4376cf7b47a4f1
deblokc <74002970+deblokc@users.noreply.github.com>
2021-12-10 21:15:16 +0100

    Update botrdel.py

--- botrdel.py
+++ botrdel.py
@@ -39,7 +39,7 @@ async def on_message(message): #quand un message est envoyé
             print(quoi)
             print(qlen)
             print(quoi[qlen-1],quoi[qlen-2],quoi[qlen-3],quoi[qlen-4])
-            if (quoi[qlen-4] == "Q" and (quoi[qlen-3]) == "U" and (quoi[qlen-2]) == "O" and (quoi[qlen-1]) == "I":
+            if (quoi[qlen-4] == "Q" and (quoi[qlen-3]) == "U" and (quoi[qlen-2]) == "O" and (quoi[qlen-1]) == "I"):
                 print("feur")
                 await message.channel.send("feur")
         if "ZIZI CACA" in message.content.upper(): #on vérifie que le bot à été ping
