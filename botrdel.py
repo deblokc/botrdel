83aafdf570378506717d2a206e00b255cbdc1ee2
Lescolin <76521652+Lescolin@users.noreply.github.com>
2022-01-18 11:34:29 +0000

    int to str

--- botrdel.py
+++ botrdel.py
@@ -36,7 +36,7 @@ async def on_message(message): #quand un message est envoyé
         if message.content.startswith("pan ") and len(message.content) > 4: #Quand le premier mot est la commande pan et suivi d'une cible
             print("PAN !")
             l = [", en plein dans le mille.", ", ça l'a touché.", ", headshot.", ", mais ça a raté.", "mais il se tire dans le pied."] #Fin de phrase
-            response = "<@" + message.author.id + "> a tiré sur " + message.content[4:] + l[randint(0, len(l) - 1)]
+            response = "<@" + str(message.author.id) + "> a tiré sur " + message.content[4:] + l[randint(0, len(l) - 1)]
             await message.channel.send(response)
         else:
             if "MILLION" in message.content.upper():
