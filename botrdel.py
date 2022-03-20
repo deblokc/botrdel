47a7692e1bec5b2c551b445fcc6fead13377ca10
deblokc <74002970+deblokc@users.noreply.github.com>
2022-03-20 23:26:54 +0100

    Update botrdel.py

--- botrdel.py
+++ botrdel.py
@@ -17,8 +17,8 @@ import re
 
 intents = discord.Intents.default()
 intents.members = True 
-client_id={os.environ.get('client_UID')}
-client_secret={os.environ.get('SECRET')}
+client_id=os.environ.get('client_UID')
+client_secret=os.environ.get('SECRET')
 DISCORD_TOKEN={os.environ.get('token')}
 DISCORD_GUILD={os.environ.get('guild')}
 Cooldown = 0
